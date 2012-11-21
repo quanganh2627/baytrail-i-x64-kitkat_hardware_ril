7d0fe819d60d2e5478510c7fb2402e8a4797b00d
Arun Ravindran <arun.ravindran@intel.com>
2012-11-21 17:29:05 +0200

    ril: Enabling seek and Smart Card API support in JB 4.2
    BZ: 47372
    
    The following changes are ported from MCG Android 4.1.2
    for enabling telephony in 4.2.
    
    1) Introduction of new RIL request for support of
       smart card API
    2) Introduction of new RIL_Errno enumerations for
       seek and smart card API.
    
    Change-Id: I1a06f5bce7eb5a1c4b3c4316553b8bfe5a321afc
    Signed-off-by: Arun Ravindran <arun.ravindran@intel.com>
    Reviewed-on: http://android.intel.com:8080/75274
    Reviewed-by: Fremaux, Pascal <pascal.fremaux@intel.com>
    Reviewed-by: Badrappan, Jeevaka <jeevaka.badrappan@intel.com>
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Reviewed-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Tested-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Reviewed-by: Protat, Remy <remy.protat@intel.com>
    Tested-by: Protat, Remy <remy.protat@intel.com>

--- include/telephony/ril.h
+++ include/telephony/ril.h
@@ -63,6 +63,8 @@ typedef enum {
     RIL_E_MISSING_RESOURCE = 17,
     RIL_E_NO_SUCH_ELEMENT = 18,
 
+    RIL_E_INVALID_PARAMETER = 19                /* SEEK for Android */
+
 } RIL_Errno;
 
 typedef enum {
@@ -273,6 +275,7 @@ typedef struct {
 } RIL_Dial;
 
 typedef struct {
+    int cla;
     int command;    /* one of the commands listed for TS 27.007 +CRSM*/
     int fileid;     /* EF id */
     char *path;     /* "pathid" from TS 27.007 +CRSM command.
@@ -287,6 +290,7 @@ typedef struct {
 } RIL_SIM_IO_v5;
 
 typedef struct {
+    int cla;
     int command;    /* one of the commands listed for TS 27.007 +CRSM*/
     int fileid;     /* EF id */
     char *path;     /* "pathid" from TS 27.007 +CRSM command.
@@ -3328,6 +3332,22 @@ typedef struct {
  *  GENERIC_FAILURE
  */
 #define RIL_REQUEST_VOICE_RADIO_TECH 108
+
+// "data" is a const RIL_SIM_IO *
+// "response" is a const RIL_SIM_IO_Response *
+#define RIL_REQUEST_SIM_TRANSMIT_BASIC 109
+
+// "data" is a const char * containing the AID of the applet
+// "response" is a int * containing the channel id
+#define RIL_REQUEST_SIM_OPEN_CHANNEL 110
+
+// "data" is a const int * containing the channel id
+// "response" is NULL
+#define RIL_REQUEST_SIM_CLOSE_CHANNEL 111
+
+// "data" is a const RIL_SIM_IO *
+// "response" is a const RIL_SIM_IO_Response *
+#define RIL_REQUEST_SIM_TRANSMIT_CHANNEL 112
 
 /***********************************************************************/
 
